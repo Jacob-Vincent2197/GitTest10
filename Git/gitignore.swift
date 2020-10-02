70994e32ac75457f30e2b67c60cacee79ab43584
Vincent, Jacob <Jacob.vincent.151@k12.friscoisd.org>
2020-10-02 11:11:26 -0500

    Add .gitignore file

---
+++ Git/gitignore.swift
@@ -0,0 +1,13 @@
+//
+//  gitignore.swift
+//  GitTest10
+//
+//  Created by Vincent, Jacob M on 10/2/20.
+//  Copyright © 2020 Vincent, Jacob M. All rights reserved.
+//
+
+import UIKit
+
+class gitignore: NSObject {
+
+}
